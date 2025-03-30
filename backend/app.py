abf32e8d289c9ad150c39dbafdcdaabe2c145ab5
June Eguilos <tulcusss@gmail.com>
2025-03-29 20:54:49 -0700

    flask to react connection test

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
+from flask_cors import CORS
 #test
 import time
 
@@ -12,23 +13,25 @@ def create_app():
     app.config['SECRET_KEY'] = 'secret!'
     socketio.init_app(app)
 
+    CORS(app, )
+
     @app.route('/time')
-    def getCurrentTime():
-        return {'time', time.time()}
-    
+    def get_time():
+        current_time = { 'hour': 10, 'minute': 30 }
+        return {'time': time.time()}
     return app
 
 
 
 #Messages are received by both parties as events. 
-@socketio.on('message')
-def handle_message(data):
-    print('Received message: ' + data)
+# @socketio.on('message')
+# def handle_message(data):
+#     print('Received message: ' + data)
 
-# sending Messages
-@socketio.on('message')
-def handle_message(message):
-    send(message)
+# # sending Messages
+# @socketio.on('message')
+# def handle_message(message):
+#     send(message)
 
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/time')
+      .then(res => res.json())
+      .then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
 
   return (
     <>
@@ -28,6 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p> The time is: {currentTime} </p>
     </>
   )
 }
